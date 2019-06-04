e1a1783a3d215840b2bcf05c64c6063da1b4fd6a
Lluís Montabes <lluis.456@gmail.com>
2019-06-04 15:50:42 +0200

    Added empty url redirect

--- PracticaWeb/urls.py
+++ PracticaWeb/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.conf import settings
 
 urlpatterns = [
+    url(r'', include('forkilla.urls')),
     url(r'^forkilla/', include('forkilla.urls')),
     url(r'^admin/', admin.site.urls),
 ]
